ada2fe2ad576f203dff42c945f243f0e3698295b
rhombus <rhom6us@users.noreply.github.com>
2025-10-19 23:22:22 -0400

    added some claude profiles to windows terminal

--- configs/install.ps1
+++ configs/install.ps1
@@ -1,7 +1,7 @@
 ﻿
 function New-Link ([string]$Target, [string]$Link) {
     if (Test-Path $Link) {
-        Write-Warning "`"$Link`" already exists. Backing up to `"$Link.bak`" and removing"
+        Write-Warning "`"$Link`" already exists. Backing up to `"$Link.bak`""
         Move-Item -Path "$Link" -Destination "$Link.bak" -Force
     }
     New-Item -Path "$Link" -ItemType SymbolicLink -Value "$Target"
@@ -9,7 +9,7 @@ function New-Link ([string]$Target, [string]$Link) {
 
 $configs = "$env:CALLISTO_HOME\configs"
 
-New-Link "$configs\pwsh\profile.ps1" $profile.AllUsersAllHosts
+New-Link "$configs\pwsh\profile.ps1" $profile.CurrentUserAllHosts
 New-Link "$configs\clink\oh-my-posh.lua" "$env:LOCALAPPDATA\clink\oh-my-posh.lua"
 New-Link "$configs\windows.terminal\settings.json" "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
 
